584a357614570d28b7d69c308bb1d210c0f4771a
Almas Kureshi <almaskureshi099@gmail.com>
2026-05-21 08:18:44 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,63 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
 const connectDb = require("./config/db");
-const authRoutes = require('./routes/authRoutes')
-const userRoutes = require('./routes/userRoutes')
-const taskRoutes = require('./routes/taskRoutes')
-const reportRoutes = require('./routes/reportRoutes')
-const aiRoutes = require('./routes/aiRoutes')
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+const aiRoutes = require("./routes/aiRoutes");
+
 const app = express();
 
 
-//Middleware to handle CORS
+// Connect Database
+connectDb();
+
+
+// Middleware
+app.use(express.json());
+
+
+// CORS Middleware
 app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
+        credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
 
-// connect database 
-connectDb();
-
-//Middleware
-app.use(express.json())
-
-
-
-//Routes
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
-app.use("/api/ai", aiRoutes)
+app.use("/api/ai", aiRoutes);
 
-// server upload folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-//Start server
-const PORT = process.env.PORT || 5000;
+// Static Upload Folder
+app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "uploads"))
+);
+
+
+// Test Route
 app.get("/", (req, res) => {
     res.send("EMS Backend Running Successfully");
 });
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
